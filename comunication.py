1324e398153fb9780c6aecb945b30a4c88d669da
Oleksii-Senchenko <alex.sench7@gmail.com>
2025-06-21 22:22:04 +0200

    added: help command. simplified the visualization of the code

---
+++ comunication.py
@@ -0,0 +1,6 @@
+start_text = "Nice to see you here!\n\nType /help to get information about what I can do:)"
+help_text = ("To reach your goal, you should type the amount and "
+             "\nthen the currency you want to convert from."
+             "\n\nThen you will be able to choose the currency you want to convert to."
+             "\n\nHere is an example of a request: 980 UAH\nPlease pay attention that the first part is the digits (amount),\nand the second part is the uppercase currency code.")
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 import requests
 from dotenv import load_dotenv
-
+from comunication import start_text, help_text
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
@@ -11,7 +11,12 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=["start"])
 def start_handler(message: telebot.types.Message):
     return bot.send_message(message.chat.id,
-                            f"Hello {message.from_user.first_name}. Nice to see you here!\n\nType /help to get information about what I can do:)")
+                            f"Hello {message.from_user.first_name}. {start_text}")
+
+@bot.message_handler(commands=["help"])
+def help_handler(message: telebot.types.Message):
+    return bot.send_message(message.chat.id,
+                            f"{message.from_user.first_name.capitalize() }. {help_text}")
 
 
 
